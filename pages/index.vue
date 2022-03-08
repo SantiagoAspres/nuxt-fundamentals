6295ac09334cae3c28242fbf82b0371fd84058cc
SantiagoAspres <48845712+SantiagoAspres@users.noreply.github.com>
2022-03-08 15:27:57 -0300

    commit inicial

---
+++ pages/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">
+        nuxt-fundamentals
+      </h1>
+      <h2 class="subtitle">
+        My gnarly Nuxt.js project
+      </h2>
+      <div class="links">
+          <nuxt-link 
+          v-for="post in posts"
+          :to="{name: 'posts-id', params: {id: post.id}}"
+          :key="post.id"
+          class="button--grey">
+            {{post.title}}
+          </nuxt-link>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/VuetifyLogo.vue'
+export default {
+  head() {
+    return{
+      title:'Home Page',
+      meta: [
+        { name: 'twitter:title', content: 'Nuxt Fundamentals by Vue School'},
+        { name: 'twitter:description', content: 'Nuxt + Vue School = 🍕'},
+        { name: 'twitter:image', content: 'https://i.imgur.com/UYP2umJ.png'},
+        { name: 'twitter:card', content: 'summary_large_image'} // Especifica el tamaño de la imagen
+      ]
+    }
+  },
+  components: {
+    Logo
+  },
+  computed: {
+    posts(){
+      return this.$store.state.posts.all
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+  'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  letter-spacing: 1px;
+}
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+}
+.links {
+  padding-top: 15px;
+}
+</style>
